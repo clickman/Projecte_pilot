edc2c2684f41d71cb30663589ab383aef70538ad
Javi <j.hernando.sch@gmail.com>
2019-05-28 16:49:28 +0200

    add size

--- dades_iris.r
+++ dades_iris.r
@@ -4,5 +4,5 @@ require(ggplot2)
 
 # Fem un pair plot de les dades iris
 ggplot(iris) +
-  geom_point(aes(x=Sepal.Length, y=Sepal.Width, color=Species)) +
+  geom_point(aes(x=Sepal.Length, y=Sepal.Width, color=Species), size = 3) +
   theme_bw()

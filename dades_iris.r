9a1b089f38e63f2ef48d260ad0ba6eeb88a2f906
Javi <j.hernando.sch@gmail.com>
2019-05-28 17:22:03 +0200

    filtrem iris

--- dades_iris.r
+++ dades_iris.r
@@ -4,6 +4,8 @@ library(ggplot2)
 iris
 require(ggplot2)
 
+iris <- iris[which(iris$Species=='versicolor'),]
+
 # Fem un pair plot de les dades iris
 ggplot(iris) +
   geom_point(aes(x=Sepal.Length, y=Sepal.Width, color=Species), size = 3) +

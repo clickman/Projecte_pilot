55d93fb46313d60e7c27f3a9534eafc745060e25
Josep M <jmcarreras@minsait.com>
2019-05-28 16:36:50 +0200

    En ggplot2

--- dades_iris.r
+++ dades_iris.r
@@ -1,5 +1,11 @@
+library(ggplot2)
+
 # Utilitzarem les dades iris
 iris
 
 # Fem un pair plot de les dades iris
 plot(iris)
+
+# Ggplot2
+ggplot(iris) + 
+  geom_point(aes(x = Sepal.Length, y = Sepal.Width, col = Species))
